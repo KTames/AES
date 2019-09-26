3a7089d23fcc9594d5365c41e65f3f602c0f1b07
Steven Quesada <stevenq161198@gmail.com>
2019-09-26 12:41:00 -0600

    Update to latest version of Kendall's branch
    Update AES.java and PasswordFinder.java

--- AES.java
+++ AES.java
@@ -12,7 +12,8 @@ public class AES {
     private static SecretKeySpec secretKey;
     private static byte[] key;
 
-    public static void setKey(String myKey) {
+    // Hace un set del secret key, actualiza el secret key cada vez que se llama para hacer la prueba
+    private static void setKey(String myKey) {
         MessageDigest sha = null;
         try {
             key = myKey.getBytes("UTF-8");
@@ -26,32 +27,17 @@ public class AES {
             e.printStackTrace();
         }
     }
-
-    public static String encrypt(String strToEncrypt, String secret) {
+    // Funcion que desencripta el mensaje oculto, necesita una llame secreta para intentarlo
+    public static String decrypt(String strToDecrypt, String secret) {
         try {
-            setKey(secret);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-        } catch (Exception e) {
-            System.out.println("Error while encrypting: " + e.toString());
-        }
-        return null;
-    }
-    
-    public static String decrypt(String strToDecrypt, String secret)
-    {
-        try
-        {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        }
-        catch (Exception e)
-        {
-            System.out.println("Error while decrypting: " + e.toString());
-        }
-        return null;
+        } catch (Exception e) {
+            return "";
+        }        
     }
+    
+
 }
--- PasswordFinder.java
+++ PasswordFinder.java
@@ -1,4 +1,29 @@
 public class PasswordFinder {
+    // Declaracion de variables fijas
 
-    
+    // Mesaje encriptado
+    static private final String ENCRYPTED_STRING = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
+    // Alfabeto completo 26 letras
+    static private final char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    // El secret key incompleto
+    static private final String[] INCOMPLETE_KEY = { "29dh120", "dk1", "3" };
+    // Los numeros del 0 al 9
+    static private final char[] NUMBERS = "0123456789".toCharArray();
+
+    // Crea todas las combinaciones de las 2 listas de elementos (letras y numeros)
+    private static String[] getFullCombinations(char[] letters, char[] numbers) {
+        int combinacionesTotales = letters.length * numbers.length;
+
+        String[] combinaciones = new String[combinacionesTotales];
+
+        int indiceCombinaciones = 0;
+        // Combina las letras con los numeros, para dar una posible combinacion correcta ((1/26)+(1/10))/260
+        for (int letterIndex = 0; letterIndex < letters.length; letterIndex++)
+            for (int numberIndex = 0; numberIndex < numbers.length; numberIndex++)
+                combinaciones[indiceCombinaciones++] = letters[letterIndex] + "-" + numbers[numberIndex];
+        return combinaciones;
+    }
+
+    public static void run() {}
+
 }
