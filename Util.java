875c661db2b2abfb9190752d406643284d3068a4
Steven Quesada <stevenq161198@gmail.com>
2019-09-26 12:20:19 -0600

    Add 2 functions of scramble
    To use scramble  letters and numbers, in different types: char and string

--- Util.java
+++ Util.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Util {
+    // Declaracion de variables fijas 
+    static private final Random random = new Random();
     
     // Verifica si el rsultado del dencrypt es imprimible o no, puede salir basura
     // (caracteres no imprimibles o un error que retorna null)
@@ -19,4 +21,30 @@ public class Util {
 
         System.out.println(" ]");
     }
+    // Hace Scramble a un array de chars
+    public static char[] scrambleCharArray(char[] characters, int length) {
+        char[] charactersCopy = Arrays.copyOf(characters, characters.length);
+        char[] output = new char[length];
+        int originalLength = length;
+        //empieza a modificar los campos de los elementos, para hacerlos random
+        for (int charPosition = 0; charPosition < originalLength; charPosition++) {
+            int randomIndex = random.nextInt(length--);
+            output[charPosition] = charactersCopy[randomIndex];
+            charactersCopy[randomIndex] = charactersCopy[length];
+        }
+
+        return output;
+    }
+    // Hace Scramble a un array de strings
+    public static String[] scrambleStringArray(String[] characters, int length) {
+        String[] charactersCopy = Arrays.copyOf(characters, characters.length);
+
+        String[] output = new String[length];
+        int originalLength = length;
+        //empieza a modificar los campos de los elementos, para hacerlos random
+        for (int charPosition = 0; charPosition < originalLength; charPosition++) {
+            int randomIndex = random.nextInt(length--);
+            output[charPosition] = charactersCopy[randomIndex];
+            charactersCopy[randomIndex] = charactersCopy[length];
+        }
 }
